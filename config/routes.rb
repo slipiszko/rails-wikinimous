09775420d03cfae0667a235aa19bc2f2da154147
Sebastian Lipiszko <lipsikko@gmail.com>
2019-08-20 16:39:39 +1000

    Man I love Rails<3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+
+  get 'articles', to: 'articles#index'
+
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  delete 'articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Processing"
+
+10.times do
+  article = Article.new(
+    title: Faker::Company.name,
+    content: Faker::Address.street_address
+  )
+  article.save!
+end
+
+puts "Confirmed! #{Article.count}"
